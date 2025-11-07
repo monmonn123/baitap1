5dc1efd2da8b1f0838fc2aa462e0735762e4d6a9
monmon123 <ichigo01102005@gmail.com>
2025-11-07 09:42:05 +0700

    Upload baitap1 version cua Phuoc

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# Mobile_BT1
+ Profile Android giới thiệu Phạm Quang Vinh – Sinh viên năm 3 Ngành An toàn thông tin.
+ # Ảnh chụp màn hình
+ <img width="392" height="820" alt="image" src="https://github.com/user-attachments/assets/94b56a80-48cd-41b3-91f2-e88cec0fb382" />
+
+# Cài đặt & Chạy
+1. Cài đặt Android Studio
+2. Clone repo và mở thư mục gốc bằng Android Studio.
+3. Chọn AVD (Device Manager → Create device…) hoặc điện thoại thật rồi Run ▶.
+
---
+++ app/src/main/java/com/quangvinh/bt1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.quangvinh.bt1
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
